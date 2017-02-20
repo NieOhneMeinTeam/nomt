375b22efbfbd794a14e16036d61923b8bde9b659
jbrzakala <jbrzakala@student.tgm.ac.at>
2017-02-20 12:53:21 +0100

    S.o.p eingefügt -Julian

--- src/chelapurath/NieOhneMeinTeam.java
+++ src/chelapurath/NieOhneMeinTeam.java
@@ -5,6 +5,7 @@ public class NieOhneMeinTeam {
 	public static void main (String[] args) {
 		System.out.println("Schöne Grüße von jchelapurath - Nie ohne mein Team!");
 		System.out.println("Mario Fentler war hier");
+		System.out.println("Julian Brzakala ist der Gruppe beigetreten :D");
 	}
 
 }
