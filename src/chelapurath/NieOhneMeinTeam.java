791164488856078c2c5a92d04240099c5b42722b
mfentler <mfentler@student.tgm.ac.at>
2017-02-20 11:27:55 +0100

    Consolenausgabe hinzugefügt

--- src/chelapurath/NieOhneMeinTeam.java
+++ src/chelapurath/NieOhneMeinTeam.java
@@ -3,7 +3,8 @@ package chelapurath;
 public class NieOhneMeinTeam {
 	
 	public static void main (String[] args) {
-		System.out.print("Schöne Grüße von jchelapurath - Nie ohne mein Team!");
+		System.out.println("Schöne Grüße von jchelapurath - Nie ohne mein Team!");
+		System.out.println("Mario Fentler war hier");
 	}
 
 }
